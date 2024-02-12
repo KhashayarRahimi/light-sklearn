00f72a2e06c65d3224b5c808d821c27a68245551
KhashayarRahimi <khashayar1kaggle@gmaile.com>
2024-02-12 22:01:11 +0330

    KNN regressor and classifier finished

--- KNearestNeighbors.py
+++ KNearestNeighbors.py
@@ -54,13 +54,28 @@ class KNN:
 
             LR = Linear_Regression.LinearRegression(Local_x,Local_y,learning_rate=0.1)#scaled_x,scaled_y)
 
-            coef = LR.fit()
-            print('x_sample',x_sample,type(x_sample),x_sample.shape,np.array(x_sample.reshape(1,3)))
+            coef = LR.fit(Coef = False)
+            #print('x_sample',x_sample,type(x_sample),x_sample.shape,np.array(x_sample.reshape(1,3)))
 
-            pre = LR.predict(x_sample)
+            pre = LR.predict(np.array([x_sample]))
             return pre
         
+            """
+            elif self.task == 'regression':
 
+                Sum = 0
+
+                sumOfDist = 0
+
+                for indx in k_nearest_neightbor:
+
+                    Sum +=   k_nearest_neightbor_label[indx] * (Sorted_Distances[indx])
+
+                    sumOfDist += Sorted_Distances[indx]
+                
+
+                return np.mean([k_nearest_neightbor_label[indx] for indx in k_nearest_neightbor])#Sum/sumOfDist
+           """
 
         else:
 
@@ -99,28 +114,6 @@ class KNN:
 
 
 
-            
-
-
-
-        """        elif self.task == 'regression':
-
-            Sum = 0
-
-            sumOfDist = 0
-
-            for indx in k_nearest_neightbor:
-
-                Sum +=   k_nearest_neightbor_label[indx] * (Sorted_Distances[indx])
-
-                sumOfDist += Sorted_Distances[indx]
-            
-
-            return np.mean([k_nearest_neightbor_label[indx] for indx in k_nearest_neightbor])#Sum/sumOfDist
-        """
-
-
-
 
 """
 from sklearn.datasets import make_classification
@@ -152,10 +145,10 @@ KNN = KNearestNeighbors.KNN(task = 'classification')
 
 y_pred = KNN.predict(X_train,y_train,X_test)
 
-print(KNN.accuracy(y_test,y_pred))
+print(KNN.accuracy(y_test,y_pred)) ----> 0.95
 
 
--------------------------------
+===============================================================
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -176,40 +169,12 @@ test_size = 0.3  # 30% of the data will be used for testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
 
 
-KNN = KNearestNeighbors.KNN(task = 'regression',n_neighbors=10)
+KNN = KNearestNeighbors.KNN(task = 'regression',n_neighbors=30,r=3)
 
 #KNN.fit(X_train,y_train)
 
 y_pred = KNN.predict(X_train,y_train,X_test)
 
-print(KNN.accuracy(y_test,y_pred))
-
-
-import Ridge_Regression
-from sklearn.datasets import make_regression
-from sklearn.model_selection import train_test_split
-import numpy as np
-# Generate regression data with three features
-X, y = make_regression(n_samples=1000, n_features=3, noise=0.1, random_state=42)
-
-# Add more samples
-additional_samples = 1000
-X_additional, y_additional = make_regression(n_samples=additional_samples, n_features=3, noise=0.1, random_state=42)
-
-# Concatenate additional samples to existing dataset
-X = np.vstack([X, X_additional])
-y = np.concatenate([y, y_additional])
-test_size = 0.3  # 30% of the data will be used for testing
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
-
-rr = Ridge_Regression.RidgeRegression(X_train,y_train)
-
-rr.fit()
-
-y_pred=rr.predict(X_test)
-print(rr.MAE(y_test,y_pred))
-
-
-
+print(KNN.accuracy(y_test,y_pred)) ------> 0.091
 
 """
